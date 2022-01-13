fce956388c851a84e6434657fa70720136ab8a5e
smartcatboy <smartcatboy@tuta.io>
2022-01-13 22:47:49 +0800

    feat: sync foom script

--- tools.sh
+++ tools.sh
@@ -12,7 +12,6 @@ echo "\
 
 Linux网络优化脚本 (生产环境慎用)
 
-教程: http://relay.nekoneko.cloud/knowledge/Linux网络优化
 上次更新: 2021-10-27
 
 Powered by Neko Neko Cloud
@@ -41,24 +40,59 @@ sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 cat >> /etc/sysctl.conf << EOF
-net.ipv4.tcp_no_metrics_save=1
-net.ipv4.tcp_ecn=0
-net.ipv4.tcp_frto=0
-net.ipv4.tcp_mtu_probing=0
-net.ipv4.tcp_rfc1337=0
-net.ipv4.tcp_sack=1
-net.ipv4.tcp_fack=1
-net.ipv4.tcp_window_scaling=1
-net.ipv4.tcp_adv_win_scale=1
-net.ipv4.tcp_moderate_rcvbuf=1
-net.core.rmem_max=16777216
-net.core.wmem_max=16777216
-net.ipv4.tcp_rmem=4096 87380 16777216
-net.ipv4.tcp_wmem=4096 16384 16777216
-net.ipv4.udp_rmem_min=8192
-net.ipv4.udp_wmem_min=8192
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
+#net.ipv4.tcp_no_metrics_save=1
+#net.ipv4.tcp_ecn=0
+#net.ipv4.tcp_frto=0
+#net.ipv4.tcp_mtu_probing=0
+#net.ipv4.tcp_rfc1337=0
+#net.ipv4.tcp_sack=1
+#net.ipv4.tcp_fack=1
+#net.ipv4.tcp_window_scaling=1
+#net.ipv4.tcp_adv_win_scale=1
+#net.ipv4.tcp_moderate_rcvbuf=1
+#net.core.rmem_max=16777216
+#net.core.wmem_max=16777216
+#net.ipv4.tcp_rmem=4096 87380 16777216
+#net.ipv4.tcp_wmem=4096 16384 16777216
+#net.ipv4.udp_rmem_min=8192
+#net.ipv4.udp_wmem_min=8192
+#net.core.default_qdisc=fq
+#net.ipv4.tcp_congestion_control=bbr
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+net.ipv4.ip_forward = 1
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv4.tcp_no_metrics_save = 1
+net.ipv4.tcp_ecn = 0
+net.ipv4.tcp_frto = 0
+net.ipv4.tcp_mtu_probing = 0
+net.ipv4.tcp_rfc1337 = 0
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = 1
+net.ipv4.tcp_moderate_rcvbuf = 1
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 16384 16777216
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
 EOF
 sysctl -p && sysctl --system
 }
@@ -159,13 +193,13 @@ if uname -r|grep -q "^5."
 then
     echo "已经是 5.x 内核，不需要更新"
 else
-    wget -N "http://sh.nekoneko.cloud/bbr/bbr.sh" -O bbr.sh && bash bbr.sh
+    wget -N "https://foom.cc/PF/sh/bbr/bbr.sh" -O bbr.sh && bash bbr.sh
 fi
   
 }
 
 Update_Shell(){
-  wget -N "http://sh.nekoneko.cloud/tools.sh" -O tools.sh && chmod +x tools.sh && ./tools.sh
+  wget -N "https://foom.cc/PF/sh/bbr/tools.sh" -O tools.sh && chmod +x tools.sh && ./tools.sh
 }
 
 get_opsy() {
